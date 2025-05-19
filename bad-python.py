1d3d3c68658d342c9f1ff68cac3aa7d42d11353c
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-05-19 15:38:03 +0200

    feat: add script non PEP8 compliant (#2)

---
+++ bad-python.py
@@ -0,0 +1,18 @@
+foo = long_function_name(var_one, var_two,
+    var_three, var_four)
+
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(
+    var_one, var_two, var_three,
+    var_four):
+    print(var_one)
+
+import sys, os
+
+income = (gross_wages +
+          taxable_interest +
+          (dividends - qualified_dividends) -
+          ira_deduction -
+          student_loan_interest)
+
+bar = (0, )
